b6eac72414b28d3b3c5ee11e7a2c37a593937944
Ilgiz Saburov <barysaburov@gmail.com>
2024-08-03 13:03:38 +0300

    add cod

--- style.js
+++ style.js
@@ -1,34 +1,33 @@
-const response1 = confirm('В стакане 50% воды. Стакан на 1/2 пуст или на 1/2 полон?');
+debugger
+let userResponse = confirm('В стакане 50% воды. Стакан на 1/2 пуст или на 1/2 полон?');
+let percentage = 50;
+const step = 2;
+let newResponse = userResponse
 
-for (let i = 0; i < 4; i++) {
-    const waterGlass = 100;
-    if(response1) {
-        waterGlass /= 2;
-    } else {
-        waterGlass = waterGlass /= 2;
-        alert(`Поздравляем вы на ${optimist} % оптимист!`);
-    }
+while (newResponse === userResponse) {
+    let part = percentage /= step;
+    let question = `В стакане ${percentage} % воды. Стакан на ${Math.floor(part)}
+    пуст или на ${Math.ceil(part)} полон?`;
+    newResponse = confirm(question);
+}
+
+if (newResponse) {
+    console.log("Поздравляем, вы на " + percentage * step + "% оптимист!");
+} else {
+    console.log("Поздравляем, вы на " + percentage * step + "% пессимист!");
 }
 
 
-while () {
-    const waterGlass = 100;
-    if(firstMatter) {
-        waterGlass /= 2;
-    } else {
-        waterGlass = waterGlass /= 2;
-        alert(`Поздравляем вы на ${pesimist} % оптимист!`);
-    }
-    
-}
-
-
-const response2 = confirm('В стакане 25% воды. Стакан на 1/4 пуст или на 3/4 полон?');
 
 
 
-const response3 = confirm('В стакане 12.5% воды. Стакан на 1/8 пуст или на 7/8 полон?');
-const response4 = confirm('В стакане 6.25% воды. Стакан на 1/16 пуст или на 15/16 полон?');
+
+// const response2 = confirm('В стакане 25% воды. Стакан на 1/4 пуст или на 3/4 полон?');
+
+
+
+// const response3 = confirm('В стакане 12.5% воды. Стакан на 1/8 пуст или на 7/8 полон?');
+// const response4 = confirm('В стакане 6.25% воды. Стакан на 1/16 пуст или на 15/16 полон?');
 
 
 
