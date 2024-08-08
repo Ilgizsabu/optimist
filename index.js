3e59988294917c3bc8a39016e5d141c081a6c956
Ilgiz Saburov <barysaburov@gmail.com>
2024-08-08 18:43:04 +0300

    add cod

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-debugger
 let userResponse = confirm('В стакане 50% воды. Стакан на 1/2 пуст или на 1/2 полон?');
 let percentage = 50;
 const step = 2;
 let newResponse = userResponse
 
 while (newResponse === userResponse) {
-    let part = percentage /= step;
-    let question = `В стакане ${percentage} % воды. Стакан на ${Math.floor(part)}
-    пуст или на ${Math.ceil(part)} полон?`;
+    percentage /= step;
+    let partEmpty = 100 - percentage;
+    let question = `В стакане ${percentage}% воды. Стакан на ${Math.floor(partEmpty)}
+    пуст или на ${Math.ceil(percentage)} полон?`;
     newResponse = confirm(question);
 }
 
 if (newResponse) {
-    console.log("Поздравляем, вы на " + percentage * step + "% оптимист!");
+    alert("Поздравляем, вы на " + percentage * step + "% оптимист!");
 } else {
-    console.log("Поздравляем, вы на " + percentage * step + "% пессимист!");
+    alert("Поздравляем, вы на " + percentage * step + "% пессимист!");
 }
 
 
